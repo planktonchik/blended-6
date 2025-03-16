9f511e44177f86a958f84244d7bfb3ecf424245f
Plankton <planktonchik88@gmail.com>
2025-03-16 11:08:17 -0600

    add 2 tasks

---
+++ js/lesson-1.js
@@ -0,0 +1,11 @@
+/*"use strict";
+const number = prompt("enter number");
+if (+number === 10) {
+  alert("Вірно ");
+}
+alert("невірно");
+console.log(number); */
+const min = Math.floor(Math.random() * (59 - 0) + 0);
+if (min >= 0 && min < 15) {
+  alert;
+}
